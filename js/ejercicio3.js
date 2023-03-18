10a277481a0ddc1d38fcef27e298689492a21cb8
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-03-18 19:07:09 -0300

    se corrige el ejercicio 3, agregando nuevos arreglos

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,17 +1,23 @@
 let numerosAleatorios = [];
-let total = 0
 
-for( i = 0; i < 50; i++){
-    let dado1 = Math.round(Math.random() * (6 - 1) + 1);
-    let dado2 = Math.round(Math.random() * (6 - 1) + 1);
-    total = dado1+dado2;
-    numerosAleatorios.push(total);
-    console.log(numerosAleatorios);
-} 
 
-document.write('<h2>Lista de sumas de números aleatorios</h2>');
-document.write('<ul>')
-for(let indiceNumeros = 0; indiceNumeros < numerosAleatorios.length; indiceNumeros++){
-    document.write(`<li>${numerosAleatorios[indiceNumeros] }</li>`);
+for(i=1; i<=50; i++){
+        let dado1 = Math.round(Math.random() * (6 - 1) + 1);
+        let dado2 = Math.round(Math.random() * (6 - 1) + 1);
+        let total = dado1 + dado2;
+        numerosAleatorios.push(total);
 }
-document.write('</ul>');
+
+let sacarDuplicados = numerosAleatorios.filter((item, index) => numerosAleatorios.indexOf(item) === index);
+console.log(sacarDuplicados);
+
+let total = 0
+for (i= 0; i < sacarDuplicados.length; i++){
+    let apariciones = 0;
+    for(j=0; j < numerosAleatorios.length; j++){
+        if(sacarDuplicados(i) === numerosAleatorios(j)){
+            apariciones++
+        }
+        total(i)= apariciones
+    }
+}
