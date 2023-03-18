55db337a50c47530974544a174447747b95cabe7
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-03-18 19:31:42 -0300

    se corrige el ciclo for para mostrar el total de repeticiones de cada suma, reemplazando los parentesis de los array por corchetes

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,5 +1,5 @@
 let numerosAleatorios = [];
-
+let total = [];
 
 for(i=1; i<=50; i++){
         let dado1 = Math.round(Math.random() * (6 - 1) + 1);
@@ -11,13 +11,39 @@ for(i=1; i<=50; i++){
 let sacarDuplicados = numerosAleatorios.filter((item, index) => numerosAleatorios.indexOf(item) === index);
 console.log(sacarDuplicados);
 
-let total = 0
-for (i= 0; i < sacarDuplicados.length; i++){
-    let apariciones = 0;
-    for(j=0; j < numerosAleatorios.length; j++){
-        if(sacarDuplicados(i) === numerosAleatorios(j)){
-            apariciones++
-        }
-        total(i)= apariciones
-    }
+
+for (i = 0; i < sacarDuplicados.length; i++) {
+  let apariciones = 0;
+  for (j = 0; j < numerosAleatorios.length; j++) {
+    if (sacarDuplicados[i] === numerosAleatorios[j]) {
+        apariciones++;
+  }
+  total[i] = apariciones;
 }
+}
+console.log(total)
+
+/*
+document.write(`<table>
+<thead>
+    <tr>
+        <th>Suma de los dados</th>
+        <th>Cantidad de apariciones de esa suma</th>
+    </tr>
+</thead>
+<tbody>`);
+
+for(i=0; i < sacarDuplicados.length; i++){
+    document.write(
+        `<tr>
+             <td>${sacarDuplicados[i]}</td>
+             <td>${total[i]}</td>
+        </tr>`
+    )
+}
+
+document.write(`
+    </tbody>
+</table>`);
+
+*/
